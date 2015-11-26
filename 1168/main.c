83b9266e344267240ba313d515418a1114ed7ac0
EWERTONCOSTA <ewertonxcosta@gmail.com>
2015-11-26 00:00:41 -0200

    teste
    teste

--- 1168/main.c
+++ 1168/main.c
@@ -12,5 +12,50 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+int main() {
+    int N, i,j, qtdleds;
+    char led[1000];
+    
+    scanf("%d",&N);
+    for(j=1;j<=N;j++){
+        qtdleds=0;
+    
+        scanf("%s",led);
+        for(i=0;i<strlen(led);i++){
+            if(led[i]=='1'){
+                qtdleds+=2;
+            }
+            if(led[i]=='2'){
+                qtdleds+=5;
+            }
+            if(led[i]=='3'){
+                qtdleds+=5;
+            }
+            if(led[i]=='4'){
+                qtdleds+=4;
+            }
+            if(led[i]=='5'){
+                qtdleds+=5;
+            }
+            if(led[i]=='6'){
+                qtdleds+=6;
+            }
+            if(led[i]=='7'){
+                qtdleds+=3;
+            }
+            if(led[i]=='8'){
+                qtdleds+=7;
+            }
+            if(led[i]=='9'){
+                qtdleds+=6;
+            }
+            if(led[i]=='0'){
+                qtdleds+=6;
+            }
+            
+        }
+        printf("%d leds\n",qtdleds);
+    }
+    
     return 0;
 }
